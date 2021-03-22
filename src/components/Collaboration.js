b2f19c8b91e6744de9df3cd6026c22b1157f155f
AnastasiaPetrova1 <petrovanastya.98@mail.ru>
2021-03-22 21:57:05 +0300

    added a collaboration block

--- src/components/Collaboration.js
+++ src/components/Collaboration.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import image from '../../public/images/image 8.png';
 
 function Collaboration() {
   return(
     <div className='collaboration'>
-        <p>Immerse yourself in a world of possibilities</p>
+        <h1 className='possibilities'>Immerse yourself in a world of possibilities</h1>
+		<h2 className='technologies'>We use the latest technologies to create the most comfortable platform for finding business partners</h2>
+		<h3 className='why'>Why</h3>
+		<h4 className='community'>We believe in collaboration, transparency, and the power of information. Thus we united multiple national associations and prominent community members. We created the most thorough database of companies involved in the blockchain field. We added The VR Expo and conference. Bonus for participants of the Global Blockchain Network is an HR section where you can find a professional to scale up your team.</h4>
+		<h3 className='title_2'>What is it</h3>
+		<h4 className='subtitle_2'>What Easiest way to know «Who is Who in the Blockchain».</h4>
+		<h3 className='title_3'>For whom</h3>
+		<h4 className='subtitle_3'>Whether you are new to blockchain or already a prominent community member, you will find here partners, technologies, and implementation cases for any use. The Network is for everyone fascinated by technology.</h4>
+		
+		<div className='collaboration_img'>
+			<img src={image}/>
+		</div>
     </div>
+	
+	
   )
 }
 
