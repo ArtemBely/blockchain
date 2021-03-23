77a4574cc7d3f714e4872d7bd340c7c61cc0783a
AnastasiaPetrova1 <petrovanastya.98@mail.ru>
2021-03-23 17:52:24 +0300

    added block Table1

--- src/components/Table1.js
+++ src/components/Table1.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import table from '../../public/images/Group 167.png';
 
 
 function Table1() {
     return (
       <div className='table1'>
-        table
+        <img src={table} id='table_1'/>
+		<div className='table_glass'></div>
+		
+		<NavLink to='#' id='full_database'>Full database</NavLink>
+        <NavLink to='#' id='register'>Register a company</NavLink>
+		<p className='table_text'>Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum.</p>
       </div>
     )
 }
