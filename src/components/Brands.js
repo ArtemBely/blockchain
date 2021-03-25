99390e1b2eda4518d4f1923a9946531270ae4322
AnastasiaPetrova1 <petrovanastya.98@mail.ru>
2021-03-25 23:16:20 +0300

    added block a brands

--- src/components/Brands.js
+++ src/components/Brands.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import stand from '../../public/images/Rectangle 88.png';
 
 
 function Brands() {
     return (
-      <div className='brands'>
-        brands
-      </div>
+    <div className='brands'>
+		<p className='info_brands'>
+			<p className='VR_Stand'>Brand your own VR Stand</p>
+			<p className='brands_text'>Do one more step and see your business is in virtual reality. Real feeling, real people, easy access from any device or location. VR GBN is more than an event. Watch a promo, get a feeling, make it real.</p>
+			<NavLink to='#' className='seeAll_2'>See all →</NavLink>
+		</p>
+		
+		<div className='stand_one'>
+			<img src={stand} id='img_stand' />
+			<NavLink to='#' className='reserve'>Reserve</NavLink>
+			<NavLink to='#' className='discover_more'>Discover more</NavLink>
+		</div>
+		
+		<div className='stand_two'>
+			<img src={stand} id='img_stand' />
+		</div>
+    </div>
     )
 }
 
