57985ddb6861ddeddb30b8b31177413089c9d80b
AnastasiaPetrova1 <petrovanastya.98@mail.ru>
2021-03-27 22:29:00 +0300

    1

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import circle1 from '../../public/images/Ellipse 47.png';
+import circle2 from '../../public/images/Ellipse 48.png';
 
 function Footer() {
     return (
-        <div className='footer'>
-            Footer
-        </div>
+        
+            <div className='footer'>
+				
+			</div>
+        
     )
 }
 
