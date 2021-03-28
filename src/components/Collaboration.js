00aed4a5ba4438437afc4e0ad0b145a2eee7064e
AnastasiaPetrova1 <petrovanastya.98@mail.ru>
2021-03-28 15:42:40 +0300

    adaptive Collaboration and Table1

--- src/components/Collaboration.js
+++ src/components/Collaboration.js
@@ -260,7 +260,7 @@ class Collaboration extends React.Component {
     return(
       <div className='collaboration'>
           <h1 className='possibilities'>Immerse yourself in a world of possibilities</h1>
-      <h2 className='technologies'>We use the latest technologies to create the most comfortable platform for finding business partners</h2>
+      <h2 className='technologies'>We use the latest technologies to create <br/> the most comfortable platform <br/> for finding business partners</h2>
       <h3 className='why'>Why</h3>
       <h4 className='community'>We believe in collaboration, transparency, and the power of information. Thus we united multiple national associations and prominent community members. We created the most thorough database of companies involved in the blockchain field. We added The VR Expo and conference. Bonus for participants of the Global Blockchain Network is an HR section where you can find a professional to scale up your team.</h4>
       <h3 className='title_2'>What is it</h3>
