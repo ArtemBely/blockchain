0616ba5f31fe9b3f636b003967bbb561a58c55f9
Артем Белышев <artembelysev@MacBook-Pro-Artem.local>
2021-07-16 14:00:54 +0200

    changed port

--- src/server/index.js
+++ src/server/index.js
@@ -87,4 +87,4 @@ app.use((req, res, next) => {  //<-- заменить если появится 
      next (err);
 });
 
-app.listen(2345, () => {console.log('connected!')});
+app.listen(3000, () => {console.log('connected!')});
